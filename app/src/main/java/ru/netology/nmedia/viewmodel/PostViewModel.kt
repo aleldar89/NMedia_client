99fcf912af5ffe07ee475cb50b9bcadceeca91e9
Эльдар Алиев <alel29643@gmail.com>
2022-08-04 23:39:59 +0300

    First commit

---
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -0,0 +1,123 @@
+package ru.netology.nmedia.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.model.FeedModel
+import ru.netology.nmedia.repository.*
+import ru.netology.nmedia.util.SingleLiveEvent
+import java.io.IOException
+import kotlin.concurrent.thread
+
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    likes = 0,
+    published = ""
+)
+
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    // упрощённый вариант
+    private val repository: PostRepository = PostRepositoryImpl()
+    private val _data = MutableLiveData(FeedModel())
+    val data: LiveData<FeedModel>
+        get() = _data
+    val edited = MutableLiveData(empty)
+    private val _postCreated = SingleLiveEvent<Unit>()
+    val postCreated: LiveData<Unit>
+        get() = _postCreated
+
+    init {
+        loadPosts()
+    }
+
+    fun loadPosts() {
+        thread {
+            // Начинаем загрузку
+            _data.postValue(FeedModel(loading = true))
+            try {
+                // Данные успешно получены
+                val posts = repository.getAll()
+                FeedModel(posts = posts, empty = posts.isEmpty())
+            } catch (e: IOException) {
+                // Получена ошибка
+                FeedModel(error = true)
+            }.also {
+                _data.postValue(it)
+            }
+        }
+    }
+
+    fun save() {
+        edited.value?.let {
+            thread {
+                repository.save(it)
+                _postCreated.postValue(Unit)
+            }
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
+    fun likeById(id: Long) {
+        thread {
+            val posts = _data.value?.posts?.map {
+                if (it.id != id) it else it.copy(
+                    likedByMe = true,
+                    likes = it.likes + 1
+                )
+            }
+            _data.postValue(
+                posts?.let { _data.value?.copy(posts = it) }
+            )
+            repository.likeById(id)
+        }
+    }
+
+    fun dislikeById(id: Long) {
+        thread {
+            val posts = _data.value?.posts?.map {
+                if (it.id != id) it else it.copy(
+                    likedByMe = false,
+                    likes = it.likes - 1
+                )
+            }
+            _data.postValue(
+                posts?.let { _data.value?.copy(posts = it) }
+            )
+            repository.dislikeById(id)
+        }
+    }
+
+    fun removeById(id: Long) {
+        thread {
+            // Оптимистичная модель
+            val old = _data.value?.posts.orEmpty()
+            val posts = _data.value?.posts.orEmpty()
+                .filter { it.id != id }
+
+            _data.postValue(
+                _data.value?.copy(posts = posts, empty = posts.isEmpty())
+            )
+            try {
+                repository.removeById(id)
+            } catch (e: IOException) {
+                _data.postValue(_data.value?.copy(posts = old))
+            }
+        }
+    }
+
+}
